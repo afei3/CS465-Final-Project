8bc12c0f0ad3404d7b71c5889d9c123f071d1c63
Sruthi Jayanti <sjayan3@illinois.edu>
2021-11-06 23:12:25 -0500

    CHanged ids to be correct

--- app/src/main/java/com/example/sportinate/group_search_and_select/BrowseActivity.java
+++ app/src/main/java/com/example/sportinate/group_search_and_select/BrowseActivity.java
@@ -26,11 +26,11 @@ public class BrowseActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_groups, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_browse);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
