60880ccddd10ffb0c9ed5865109583e3453cc992
mafei <fma1@uci.edu>
2021-11-11 15:02:39 -0600

    change group creation to scroll view

--- app/src/main/java/com/example/sportinate/group_creation/GroupCreationActivity.java
+++ app/src/main/java/com/example/sportinate/group_creation/GroupCreationActivity.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -20,12 +21,17 @@ import android.widget.AdapterView.OnItemSelectedListener;
 
 public class GroupCreationActivity extends AppCompatActivity {
     String sport_name ="";
+    String date="";
+    String start_ampm="";
+    String end_ampm="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_creation);
 
+
+        //******Sport spinner ********
         Spinner sports_spinner = (Spinner) findViewById(R.id.sports_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -49,15 +55,101 @@ public class GroupCreationActivity extends AppCompatActivity {
             }
         });
 
-        Button next_button = findViewById(R.id.next_button1);
-        next_button.setOnClickListener(new View.OnClickListener() {
+
+        //******Date spinner********
+        Spinner date_spinner = (Spinner) findViewById(R.id.date_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> date_adapter = ArrayAdapter.createFromResource(this,
+                R.array.dates_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        date_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        date_spinner.setAdapter(date_adapter);
+        //Add listener for responding info
+        date_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GroupCreationActivity.this, GroupCreation2Activity.class);
-                intent.putExtra("sport_name",sport_name);
-                startActivity(intent);
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+                date = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
             }
         });
+
+
+        //****** start hour, start minute, start am/pm *****
+        EditText startHour = (EditText) findViewById(R.id.start_hour);
+        String start_hour = startHour.getText().toString();
+
+        EditText startMinute = (EditText) findViewById(R.id.start_minute);
+        String start_minute = startMinute.getText().toString();
+
+        Spinner start_time_spinner = (Spinner) findViewById(R.id.start_time_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> start_adapter = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        start_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        start_time_spinner.setAdapter(start_adapter);
+        //Add listener for responding info
+        start_time_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                start_ampm = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        //*** End hour, end minute, end am/pm
+
+
+        EditText endHour = (EditText) findViewById(R.id.end_hour);
+        String end_hour = endHour.getText().toString();
+
+        EditText endMinute = (EditText) findViewById(R.id.end_minute);
+        String end_minute = endMinute.getText().toString();
+
+        Spinner end_time_spinner = (Spinner) findViewById(R.id.end_time_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> end_adapter = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        end_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        end_time_spinner.setAdapter(end_adapter);
+        //Add listener for responding info
+        end_time_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                end_ampm = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+
+
+        Button next_button = findViewById(R.id.next_button1);
+
+
+
+
     }
 
 }
