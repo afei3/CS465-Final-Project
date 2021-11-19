cd2b5d9017efc846843725f0febf76089741681a
mafei <fma1@uci.edu>
2021-11-19 11:46:40 -0600

    fix button for profile feedback

--- app/src/main/java/com/example/sportinate/profile_creation/profile_creation_comfirm.java
+++ app/src/main/java/com/example/sportinate/profile_creation/profile_creation_comfirm.java
@@ -40,11 +40,7 @@ public class profile_creation_comfirm extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //Intent intent = new Intent(com.example.sportinate.profile_creation.profile_creation_comfirm.this, EditSportActivity.class);
-
-                Intent intent = new Intent(com.example.sportinate.profile_creation.profile_creation_comfirm.this, BrowseActivity.class);
-                intent.putExtra("fragmentNumber", 2);
-                //GroupInfo.group_test_bool = true;
+                Intent intent = new Intent(com.example.sportinate.profile_creation.profile_creation_comfirm.this, EditSportActivity.class);
 
                 startActivity(intent);
             }
